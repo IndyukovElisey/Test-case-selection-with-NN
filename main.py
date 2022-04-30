c8b1fb0dc0a58dec04a7689de37393d16ffd1e5d
indyukov-ep <tkbctq064>
2022-04-30 06:13:03 +0300

    quadratic and clothes

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 from datetime import datetime
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("TensorFlow version:", tf.__version__)
+if tf.config.list_physical_devices('GPU'):
+    print("TensorFlow **IS** using the GPU")
+else:
+    print("TensorFlow **IS NOT** using the GPU")
 
 
 def xor():
+    startTime = datetime.now()
     # Define our training input and output data with type 16 bit float
     # Each input maps to an output
-
+    startTime = datetime.now()
     X = tf.constant([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=tf.float16)
     Y = tf.constant([[0], [1], [1], [0]], dtype=tf.float16)
 
@@ -81,9 +88,324 @@ def xor():
     )
 
     print(cleanPredict(1, 0))  # Should Yield True
+    print("Time taken:", datetime.now() - startTime)
 
 
-startTime = datetime.now()
-xor()
-print("Time taken:", datetime.now() - startTime)
+def digits():
+    mnist = tf.keras.datasets.mnist
 
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
+    ])
+
+    predictions = model(x_train[:1]).numpy()
+    predictions
+
+    tf.nn.softmax(predictions).numpy()
+
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    loss_fn(y_train[:1], predictions).numpy()
+
+    model.compile(optimizer='adam',
+                  loss=loss_fn,
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+
+    model.evaluate(x_test, y_test, verbose=2)
+
+
+class Model(tf.keras.Model):
+    def __init__(self, units):
+        super().__init__()
+        self.dense1 = tf.keras.layers.Dense(units=units,
+                                            activation=tf.nn.relu,
+                                            kernel_initializer=tf.random.normal,
+                                            bias_initializer=tf.random.normal)
+        self.dense2 = tf.keras.layers.Dense(1)
+
+    def call(self, x, training=True):
+        # For Keras layers/models, implement `call` instead of `__call__`.
+        x = x[:, tf.newaxis]
+        x = self.dense1(x)
+        x = self.dense2(x)
+        return tf.squeeze(x, axis=1)
+
+
+def quadratic():
+    matplotlib.rcParams['figure.figsize'] = [9, 6]
+
+    x = tf.linspace(-2, 2, 201)
+    x = tf.cast(x, tf.float32)
+
+    def f(x):
+        y = x ** 2 + 2 * x - 5
+        return y
+
+    y = f(x) + tf.random.normal(shape=[201])
+
+    model = Model(64)
+
+    plt.plot(x.numpy(), y.numpy(), '.', label='data')
+    plt.plot(x, f(x), label='Ground truth')
+    plt.plot(x, model(x), label='Untrained predictions')
+    plt.title('Before training')
+    plt.legend()
+    plt.show()
+
+    variables = model.variables
+
+    optimizer = tf.optimizers.SGD(learning_rate=0.01)
+
+    for step in range(1000):
+        with tf.GradientTape() as tape:
+            prediction = model(x)
+            error = (y - prediction) ** 2
+            mean_error = tf.reduce_mean(error)
+        gradient = tape.gradient(mean_error, variables)
+        optimizer.apply_gradients(zip(gradient, variables))
+
+        if step % 100 == 0:
+            print(f'Mean squared error: {mean_error.numpy():0.3f}')
+
+    plt.plot(x.numpy(), y.numpy(), '.', label="data")
+    plt.plot(x, f(x), label='Ground truth')
+    plt.plot(x, model(x), label='Trained predictions')
+    plt.title('After training')
+    plt.legend()
+    plt.show()
+
+    new_model = Model(64)
+    new_model.compile(
+        loss=tf.keras.losses.MSE,
+        optimizer=tf.optimizers.SGD(learning_rate=0.01))
+
+    history = new_model.fit(x, y,
+                            epochs=100,
+                            batch_size=32,
+                            verbose=0)
+
+    plt.plot(history.history['loss'])
+    plt.xlabel('Epoch')
+    plt.ylim([0, max(plt.ylim())])
+    plt.ylabel('Loss [Mean Squared Error]')
+    plt.title('Keras training progress')
+    plt.show()
+
+
+def clothesBuild():
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+    train_images = train_images / 255.0
+
+    test_images = test_images / 255.0
+
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+    plt.show()
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=10)
+    model.save('saved_model/my_model.h5')
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+    print('\nTest accuracy:', test_acc)
+
+
+def plot_image(i, predictions_array, true_label, img):
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10), labels=class_names, rotation=45)
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+def clothesTest():
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+    train_images = train_images / 255.0
+
+    test_images = test_images / 255.0
+
+    plt.figure(figsize=(10, 10))
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=10)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+    print('\nTest accuracy:', test_acc)
+
+    probability_model = tf.keras.Sequential([model,
+                                             tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(test_images)
+
+    predictions[0]
+
+    np.argmax(predictions[0])
+    test_labels[0]
+
+    i = 0
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i, predictions[i], test_labels)
+    plt.show()
+
+    i = 12
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i, predictions[i], test_labels)
+    plt.show()
+
+    # Plot the first X test images, their predicted labels, and the true labels.
+    # Color correct predictions in blue and incorrect predictions in red.
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows * num_cols
+    plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+        plot_image(i, predictions[i], test_labels, test_images)
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+        plot_value_array(i, predictions[i], test_labels)
+    plt.tight_layout()
+    plt.show()
+
+    # Grab an image from the test dataset.
+    img = test_images[1]
+
+    print(img.shape)
+
+    # Add the image to a batch where it's the only member.
+    img = (np.expand_dims(img, 0))
+
+    print(img.shape)
+
+    predictions_single = probability_model.predict(img)
+
+    print(predictions_single)
+
+    plot_value_array(1, predictions_single[0], test_labels)
+    _ = plt.xticks(range(10), class_names, rotation=45)
+    plt.show()
+
+    np.argmax(predictions_single[0])
+
+
+def clothesPredict(i):
+    if i < 0 or i > 9999:
+        print('out of range')
+        return
+
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+    test_images = test_images / 255.0
+
+    new_model = tf.keras.models.load_model('saved_model/my_model.h5')
+    new_model.summary()
+
+    loss, acc = new_model.evaluate(test_images, test_labels, verbose=2)
+    print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
+    print(new_model.predict(test_images).shape)
+
+    probability_model = tf.keras.Sequential([new_model,
+                                             tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(test_images)
+
+    print(predictions[i])
+
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i, predictions[i], test_labels)
+    plt.show()
+
+
+# xor()
+# digits()
+# quadratic()
+# clothesTest()
+# clothesBuild()
+i = 0
+while i < 10000:
+    clothesPredict(i)
+    i += 1
